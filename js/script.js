8fb63edbdc20557b27578315d2ee9d169fb78081
idv-loper <65097583+idv-loper@users.noreply.github.com>
2020-08-11 22:38:43 -0500

    GUI added

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function init(){
+    //obtener valores de botones
+    var resultado = document.getElementById("resultado");
+
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var division = document.getElementById("division");
+
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var multiplicacion = document.getElementById("multiplicacion");
+
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var resta = document.getElementById("resta");
+
+    var igual = document.getElementById("igual");
+    var reset = document.getElementById("reset");
+    var cero = document.getElementById("cero");
+    var suma = document.getElementById("suma");
+    
+}
